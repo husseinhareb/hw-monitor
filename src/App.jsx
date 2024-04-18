57e74333d3cdf267a7c6df2734467f492994d541
hussein <shtam.hareb12@gmail.com>
2024-04-18 22:49:59 +0300

    font-end print

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-
+import "./components/Proc";
+import Proc from "./components/Proc";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
 
-  async function greet() {
-    try {
-      const processes = await invoke("get_processes");
-      console.log(processes); // For debugging purposes
-      // Update state with the received data
-      // You can modify this part based on your requirement
-      setGreetMsg(JSON.stringify(processes));
-    } catch (error) {
-      console.error(error);
-      setGreetMsg("Error: Failed to fetch process information");
-    }
-  }
-  
+
   return (
     <div className="container">
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <Proc />
     </div>
   );
 }
---
+++ src/components/Proc.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { invoke } from "@tauri-apps/api/tauri";
+
+function Proc() {
+  const [greetMsg, setGreetMsg] = useState('');
+  const [processes, setProcesses] = useState([]);
+
+  async function getProcesses() {
+    try {
+      const processes = await invoke("get_processes");
+      console.log(processes); // For debugging purposes
+      setProcesses(processes);
+    } catch (error) {
+      console.error(error);
+      setGreetMsg("Error: Failed to fetch process information");
+    }
+  }
+
+  return (
+    <div>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          getProcesses();
+        }}
+      >
+        <button type="submit">Fetch Processes</button>
+      </form>
+
+      <p>{greetMsg}</p>
+
+      <table>
+        <thead>
+          <tr>
+            <th>pid</th>
+            <th>ppid</th>
+            <th>name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {processes.map((process, index) => (
+            <tr key={index}>
+              <td>{process.pid}</td>
+              <td>{process.ppid}</td>
+              <td>{process.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Proc;
